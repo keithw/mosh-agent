023d081ba905979dad76e36f0ad634cd7fc12987
Keith Winstein <keithw@cs.stanford.edu>
2017-02-21 03:11:33 -0800

    Skeleton of connection handling

--- src/frontend/agent-passthrough.cc
+++ src/frontend/agent-passthrough.cc
@@ -1,6 +1,8 @@
 /* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
 #include <cstdlib>
+#include <vector>
+#include <thread>
 
 #include "unix_stream_socket.hh"
 #include "exception.hh"
@@ -13,6 +15,36 @@ int main()
     UnixStreamSocket s;
 
     s.bind( UnixAddress( "/tmp/hello.socket" ) );
+
+    s.listen( 16 );
+
+    std::vector<thread> threads;
+
+    while ( true ) {
+      UnixStreamSocket connection = s.accept();
+
+      threads.emplace_back( [] ( UnixStreamSocket connection ) {
+          cerr << "Accepted new connection.\n";
+
+          while ( not connection.eof() ) {
+            const string str = connection.read();
+            if ( str.size() ) {
+              cout << "Got " << str.size() << " bytes:";
+              for ( const char x : str ) {
+                cout << " " << int( x );
+              }
+              cout << "\n";
+            }
+          }
+
+          cerr << "Closing connection.\n";
+          connection.close();
+        }, move( connection ) );
+    }
+
+    for ( auto & the_thread : threads ) {
+      the_thread.join();
+    }
 
     s.close();
 
